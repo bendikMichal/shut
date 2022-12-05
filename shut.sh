a51025ebb3880268c3af9de77d043cc5e510437d
Michal Bendik <108522291+bendikMichal@users.noreply.github.com>
2022-12-05 21:19:23 +0100

    shut

---
+++ shut.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/bash
+
+VER="0.1"
+
+if ! command -v dialog;
+	then
+		sudo pacman -S dialog
+	else
+		echo "dependencies satisfied"
+fi
+
+BLACK="\Zb\Zu\Z7"
+
+WIDTH=20
+HEIGHT=40 
+MENU_HEIGHT=8
+TITLE="SHUT"
+BG_TITLE="made by noTme"
+
+OPTIONS=( 
+	1 "Shutdown"
+	2 "Reboot"
+)
+
+CHOICE=$(dialog --colors --title "$BLACK$TITLE" --backtitle "$BG_TITLE" --menu "tired already?" "$WIDTH" "$HEIGHT" "$MENU_HEIGHT" "${OPTIONS[@]}" 2>&1 > /dev/tty)
+
+clear
+
+# echo "$CHOICE"
+
+# BEWARE OF : [ $rand ]
+#              ^     ^ must be spaces !!!
+# if [ $CHOICE -eq 1 ];
+# then
+# 	echo "shutdown"
+# elif [ $CHOICE -eq 2 ];
+# 	then
+# 		echo "reboot"	
+# fi
+
+# case better
+case $CHOICE in 
+	1) 
+		shutdown -h now;;
+	2)
+		reboot;;
+esac
